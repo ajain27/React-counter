4e21bd759f99e5fd30fcc84e5ed174d471f90fe2
Ankit Jain <ankit.jain67@t-mobile.com>
2019-08-26 10:49:08 -0700

    Adding the disabling functionality when the counter value is 0

--- src/components/Counter/counter.jsx
+++ src/components/Counter/counter.jsx
@@ -30,12 +30,13 @@ class Counter extends Component {
             <button
               onClick={() => this.props.onDecrement(this.props.counter)}
               className='btn btn-secondary btn-sm m-2'
+              disabled={this.props.counter.value === 0 ? 'disabled' : ''}
             >
               -
             </button>
             <button
               onClick={() => this.props.onDelete(this.props.counter.id)}
-              className='btn btn-danger btn-sm m-2'
+              className='btn btn-danger btn-sm'
             >
               Delete
             </button>
